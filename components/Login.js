23df95d9defef5ad5eac61ef2bf4f70795246be4
Pedro Reis Velloso dos Santos <pedro.velloso@dpu.gov.br>
2017-11-14 14:51:21 -0200

    Correção de bug

--- components/Login.js
+++ components/Login.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableHighlight, Button, Image, Modal, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TouchableHighlight, Button, Image, Modal, TextInput, Alert } from 'react-native';
 
 import styles from './Styles';
 
